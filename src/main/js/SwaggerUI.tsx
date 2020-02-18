383bce6adf31138e086b8f404e843c96bea3f079
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2020-02-18 16:48:16 +0100

    improve performance by code splitting

--- src/main/js/SwaggerUI.tsx
+++ src/main/js/SwaggerUI.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import SwaggerUIReact from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import { urls } from "@scm-manager/ui-components";
+import { apiClient, Loading, ErrorPage } from "@scm-manager/ui-components";
 import styled from "styled-components";
 
 const SwaggerUIContainer = styled.div`
   .version {
     background-color: inherit;
   }
-  
+
   .main {
     min-height: inherit;
   }
 `;
 
-export default () => (
-  <SwaggerUIContainer>
-    <SwaggerUIReact url={urls.withContextPath("/api/v2/openapi")} />
-  </SwaggerUIContainer>
-);
+const SwaggerUI: FC = () => {
+  const [spec, setSpec] = useState(undefined);
+  const [error, setError] = useState<Error | undefined>(undefined);
+  useEffect(() => {
+    apiClient
+      .get("openapi")
+      .then(response => response.json())
+      .then(setSpec)
+      .catch(setError);
+  }, []);
+
+  if (error) {
+    // TODO i18n
+    return <ErrorPage title="OpenAPI" subtitle="Failed to load OpenAPI spec" error={error} />;
+  }
+
+  if (!spec) {
+    return <Loading />;
+  }
+
+  return (
+    <SwaggerUIContainer>
+      <SwaggerUIReact spec={spec} />
+    </SwaggerUIContainer>
+  );
+};
+
+export default SwaggerUI;
--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -1,11 +1,19 @@
 import { binder } from "@scm-manager/ui-extensions";
-import React from "react";
+import { Loading } from "@scm-manager/ui-components";
+import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
-import SwaggerUI from "./SwaggerUI";
 import RestDocumentationLink from "./RestDocumentationLink";
 
 const SwaggerUIRoute = () => {
-  return <Route path="/openapi" render={() => <SwaggerUI />} />;
+  const SuspendingSwaggerUI = () => {
+    const LoadingSwaggerUI = React.lazy(() => import("./SwaggerUI"));
+    return (
+      <Suspense fallback={<Loading />}>
+        <LoadingSwaggerUI />
+      </Suspense>
+    );
+  };
+  return <Route path="/openapi" component={SuspendingSwaggerUI} />;
 };
 
 binder.bind("main.route", SwaggerUIRoute);
